1a6959b26f1f5b8cfd4a7d48087dd1076ddedfea
AshishkrGoyal <15103140ashish@gmail.com>
2017-11-09 02:43:09 +0530

    task-created

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+
+gulp.task('styles', function () {
+    console.log('Ashishkr');
+});
+
+
+gulp.task('scripts',function () {
+    console.log('Ashishkr At Starting Scripts');
+});
+
+
+gulp.task('images',function () {
+    console.log('Ashishkr At Images Task');
+});
+
+
+gulp.task('default', function () {
+    console.log('Ashishkr At Default Task');
+});
